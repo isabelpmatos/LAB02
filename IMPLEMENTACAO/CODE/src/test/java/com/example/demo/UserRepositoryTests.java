9ceec3d2b0e3a3ba9c2913354aa8bb64d6071eb8
Isabel Matos <65324450+isabelpmatos@users.noreply.github.com>
2021-09-26 20:08:42 -0300

    pt1

---
+++ IMPLEMENTACAO/CODE/src/test/java/com/example/demo/UserRepositoryTests.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback
+public class UserRepositoryTests {
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreateUser(){
+        User user = new User();
+
+        user.setEmail("isabel@gmail");
+        user.setPassword("12345");
+        user.setFirstName("Isabel");
+        user.setLastName("Matos");
+
+        User savedUser = repo.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+    }
+}
