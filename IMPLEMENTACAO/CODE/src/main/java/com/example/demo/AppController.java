ea03e40fe5a85d73f4b019a6cdb4e401a235abd8
Isabel Matos <65324450+isabelpmatos@users.noreply.github.com>
2021-09-26 23:47:54 -0300

    login

--- IMPLEMENTACAO/CODE/src/main/java/com/example/demo/AppController.java
+++ IMPLEMENTACAO/CODE/src/main/java/com/example/demo/AppController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
 @Controller
 public class AppController {
 	
@@ -35,5 +36,11 @@ public class AppController {
 	     
 	    return "register_success";
 	}
+	
+	@GetMapping("/sucesso")
+	public String sucessoPage() {
+		return "success";
+	}
+	
 	
 }
